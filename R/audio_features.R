1b7f8c9f26bc146df40d020af8e762db7cb81de9
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-10-21 12:14:17 -0500

    Update after 20th Anniversary Release

--- R/audio_features.R
+++ R/audio_features.R
@@ -3,7 +3,25 @@
 #' @md
 #' @description A collection of spotify audio features of Linkin Park Albums
 #'
-#' @format A tibble of 468 observations of 39 variables
+#' @format A tibble of 6577 observations of 39 variables
 #'
 #' See the Spotify API documentation for audio features.  https://developer.spotify.com/documentation/web-api/
+#' @export
 "audio_features"
+
+
+#' Lyrics of Linkin Park Songs
+#'
+#' @md
+#' @description A collection of genius lyrics of Linkin Park Albums
+#'
+#' @format A tibble of 8760 observations of 6 variables
+#' + `artist`: Name of the artist
+#' + `album` : Name of the album
+#' + `track_title`: Name of the track
+#' + `track_number`: Order track appears on album
+#' + `line`: Line number of lyric
+#' + `Lyric`: text of a given line
+#'
+#' @export
+"genius_lycics"
--- R/billboard_albums.R
+++ R/billboard_albums.R
@@ -9,8 +9,24 @@
 #' + `peak_position`: The highest charted position
 #' + `date_peaked`: Date album peaked on the chart (Format YYYY-MM-DD)
 #' + `weeks_on_chart`: Number of weeks the album was charted.
+#' + `chart_name`: Name of chart
+#' @export
 "billboard_albums"
 
+
+#' Peak Position of Linkin Park Songs on Billboard
+#'
+#' @md
+#' @description A data set containing peak positions of charted Linkin Park Songs
+#' from the Billboard Hard Rock streaming songs Chart. This data was scraped from billboard.com
+#'
+#' @format A tibble with 10 observations of 4 variables
+#' + `song_title`: Name of the album charted
+#' + `peak_position`: The highest charted position
+#' + `date_peaked`: Date album peaked on the chart (Format YYYY-MM-DD)
+#' + `weeks_on_chart`: Number of weeks the album was charted.
+#' @export
+"billboard_songs"
 
 
 
