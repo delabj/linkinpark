17dc766f6c728110ce6c421eead0b20b201d40bd
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-10-22 15:43:19 -0500

    Create create_internal_data.R

---
+++ data/create_internal_data.R
@@ -0,0 +1,106 @@
+tribble(
+  ~album_title,	                             ~peak_position,	~date_peaked,	~weeks_on_chart, ~chart_name,
+  "Hybrid Theory"                            ,	        	2, "2002-01-11",           	216, "Album Sales",
+  "Meteora"                                  ,            1, "2003-01-11",      	    113, "Album Sales",
+  "Minutes To Midnight"                      ,            1, "2007-06-01",	           96, "Album Sales",
+  "A Thousand Suns"                          ,            1, "2010-10-01",	           39, "Album Sales",
+  "Living Things"	                           ,            1, "2012-07-13",	           36, "Album Sales",
+  "Live In Texas"                            ,           23, "2003-12-05",	           35, "Album Sales",
+  "[Reanimation]"                            ,            2, "2002-08-19",	           33, "Album Sales",
+  "Collision Course"                         ,          	1, "2004-12-17",	           26, "Album Sales",
+  "One More Light"                           ,            1, "2017-06-09",	           24, "Album Sales",
+  "The Hunting Party"                        ,            3, "2014-07-04",	           20, "Album Sales",
+  "Meteora"                                  ,            1, "2003-04-11",            104, "Current Album Sales",
+  "Hybrid Theory"                            ,            2, "2002-01-11",            103, "Current Album Sales",
+  "Minutes To Midnight"                      ,            1, "2007-06-07",             93, "Current Album Sales",
+  "A Thousand Suns"                          ,            1, "2010-10-01",             46, "Current Album Sales",
+  "Living Things"                            ,            1, "2012-07-13",             46, "Current Album Sales",
+  "Live In Texas"                            ,           32, "2003-12-05",             35, "Current Album Sales",
+  "[Reanimation]"                            ,            2, "2002-08-16",             33, "Current Album Sales",
+  "One More Light"                           ,            1, "2017-06-09",	            29, "Current Album Sales",
+  "Collision Course"                         ,            1, "2004-12-17",             25, "Current Album Sales",
+  "The Hunting Party"                        ,            3, "2014-07-04",             24, "Current Album Sales",
+  "Recharged"                                ,           10, "2013-11-15",             13, "Current Album Sales",
+  "One More Light: Live"                     ,           17, "2018-01-02",              6, "Current Album Sales",
+  "A Thousand Suns: Puerta De Alcala (EP)"   ,          112, "2011-02-18",              1, "Current Album Sales",
+  "Songs From The Underground (EP)"          ,           96, "2008-12-12",              1, "Current Album Sales",
+  "Hybrid Theory"                            ,            2, "2017-08-11",            156, "Top Rock Albums",
+  "Minutes To Midnight"                      ,            1, "2007-06-01",             82, "Top Rock Albums",
+  "A Thousand Suns"                          ,            1, "2010-10-01",             39, "Top Rock Albums",
+  "Living Things"                            ,            1, "2012-07-13",             34, "Top Rock Albums",
+  "The Hunting Party"                        ,            1, "2014-07-04",             30, "Top Rock Albums",
+  "One More Light"                           ,            1, "2017-06-09",             29, "Top Rock Albums",
+  "Meteora"                                  ,            3, "2017-08-11",             18, "Top Rock Albums",
+  "Recharged"                                ,            3, "2013-11-15",             10, "Top Rock Albums",
+  "Road To Revolution: Live At Milton Keynes",           12, "2008-12-12",              5, "Top Rock Albums",
+  "One More Light: Live"                     ,            3, "2018-01-02",              2, "Top Rock Albums",
+  "A Thousand Suns: Puerta De Alcala (EP)"   ,           27, "2011-02-18",              1, "Top Rock Albums",
+  "Collision Course"                         ,           27, "2017-08-11",              1, "Top Rock Albums",
+  "Songs From The Underground (EP)"          ,           24, "2008-12-12",              1, "Top Rock Albums",
+  "Collision Course"                         ,            3, "2004-12-17",             17, "Top Rap Albums",
+  "Recharged"                                ,            1, "2013-11-15",             37, "Top Dance/Electronic Albums",
+  "Hybrid Theory"                            ,            3, "2017-08-11",            166, "Alternative Albums",
+  "A Thousand Suns"                          ,            1, "2010-10-01",             35, "Alternative Albums",
+  "Living Things"                            ,            1, "2012-07-13",             30, "Alternative Albums",
+  "One More Light"                           ,            1, "2017-07-09",             28, "Alternative Albums",
+  "The Hunting Party"                        ,            1, "2014-07-04",             23, "Alternative Albums",
+  "Meteora"                                  ,            4, "2017-08-11",             18, "Alternative Albums",
+  "Recharged"                                ,            3, "2013-11-15",              7, "Alternative Albums",
+  "Road To Revolution: Live At Milton Keynes",            8, "2008-12-12",              6, "Alternative Albums",
+  "Minutes To Midnight"                      ,            7, "2017-08-11",              4, "Alternative Albums",
+  "One More Light: Live"                     ,            2, "2018-01-02",              2, "Alternative Albums",
+  "Songs From The Underground (EP)"          ,           17, "2008-12-12",              1, "Alternative Albums",
+  "Collision Course"                         ,           18, "2019-08-11",              1, "Alternative Albums",
+  "A Thousand Suns: Puerta De Alcala (EP)"   ,           18, "2011-02-18",              1, "Alternative Albums",
+  "Hybrid Theory"                            ,            1, "2017-08-04",            209, "Hard Rock Albums",
+  "Meteora"                                  ,            2, "2017-08-04",            166, "Hard Rock Albums",
+  "Minutes To Midnight"                      ,            1, "2007-08-03",            129, "Hard Rock Albums",
+  "A Thousand Suns"                          ,            1, "2010-10-01",             54, "Hard Rock Albums",
+  "Living Things"                            ,            1, "2012-07-13",             52, "Hard Rock Albums",
+  "The Hunting Party"                        ,            1, "2014-07-04",             38, "Hard Rock Albums",
+  "Recharged"                                ,            1, "2013-11-15",             23, "Hard Rock Albums",
+  "Road To Revolution: Live At Milton Keynes",            6, "2008-12-12",              8, "Hard Rock Albums",
+  "One More Light: Live"                     ,            2, "2018-01-02",              6, "Hard Rock Albums",
+  "A Thousand Suns: Puerta De Alcala (EP)"   ,            9, "2011-02-18",              1, "Hard Rock Albums",
+  "Songs From The Underground (EP)"          ,           14, "2008-12-12",              1, "Hard Rock Albums",
+  "Collision Course"                         ,            9, "2017-08-11",              1, "Hard Rock Albums"
+
+
+) -> billboard_albums
+
+
+
+tribble(
+  ~trackname           , ~peak_position, ~date_peaked, ~weeks_on_chart, ~chart_name,
+  "In the End"         ,              1, "2001-12-21",              44, "Hard Rock Streaming Songs",
+  "Faint"              ,              1, "2003-08-08",              37, "Hard Rock Streaming Songs",
+  "Bleed It Out"       ,              2, "2007-09-07",              36, "Hard Rock Streaming Songs",
+  "Waiting For The End",              1, "2011-07-07",              35, "Hard Rock Streaming Songs",
+  "Crawling"           ,              5, "2001-06-29",              34, "Hard Rock Streaming Songs",
+  "One Step Closer"    ,              5, "2001-01-12",              33, "Hard Rock Streaming Songs",
+  "What I've Done"     ,              1, "2007-04-20",              31, "Hard Rock Streaming Songs",
+  "Given Up"           ,              4, "2008-06-20",              30, "Hard Rock Streaming Songs",
+  "Numb"               ,              1, "2003-11-12",              30, "Hard Rock Streaming Songs",
+  "Shadow Of The Day"  ,              2, "2008-01-25",              30, "Hard Rock Streaming Songs"
+)->billboard_songs
+
+linkinpark::riaa_lp->riaa_lp
+linkinpark:::reddit_top_100_songs->reddit_top_100_songs
+
+
+
+library(spotifyr)
+
+access_token <- get_spotify_access_token()
+
+
+get_artist_audio_features(artist = "Linkin Park")->audio_features
+
+
+audio_features %>%
+  select("artist" = artist_name,
+         "album"=album_name) %>%
+  distinct() %>%
+  genius::add_genius(artist, album, "album")-> genius_lycics
+
+usethis::use_data(billboard_albums,riaa_lp,reddit_top_100_songs,audio_features,genius_lycics,billboard_songs, internal = TRUE, overwrite = TRUE)
